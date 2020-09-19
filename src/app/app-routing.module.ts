b6fa121a513e0d98cf3e328540b3db2f2be4b7bf
elkingutierrex <elkin.gutierrex@gmail.com>
2020-09-19 09:50:18 -0500

    202009190948 - Parte visual preparada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoanApplyComponent } from './componets/credits/loan-apply/loan-apply.component';
+import { CreditsComponent } from './componets/credits/credits.component';
+import { LoanListComponent } from './componets/credits/loan-list/loan-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: CreditsComponent,
+    children:
+    [ {path: 'solicita-credito', component: LoanApplyComponent},
+      {path: 'resumen-creditos', component: LoanListComponent}
+    ]  
+  },
+  {path: '**', redirectTo: 'home'}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
